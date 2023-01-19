4de54cf2ec580dfaa9709832ef822f4e443c38a4
danissimo <danis.tazeev@gmail.com>
2023-01-19 12:00:57 +0300

    run-bot.sh: kills and deletes previous, pulls and runs new

--- src/docker/host/run-bot.sh
+++ src/docker/host/run-bot.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+
 TAG="$1"
 TOKEN="$2"
 [ -z "$TOKEN" ] && TOKEN="$ANARUCOMBOTTOKEN"
+
 if [ -z "$TAG" ] || [ -z "$TOKEN" ]; then
   BASENAME="$(basename $0)"
   [ -z "$TAG"] && MSG="TAG missing" || MSG="TOKEN undefined"
@@ -8,12 +11,32 @@ if [ -z "$TAG" ] || [ -z "$TOKEN" ]; then
   echo "Usage: $BASENAME TAG [TOKEN]"
   echo "NOTE: if TOKEN not provided ANARUCOMBOTTOKEN"
   echo "      is used (which might be empty too)"
-  exit -1
+  exit 127
 fi
+
+IMAGE="danissimo/anarucombot:$TAG"
+CONT=anarucombot
+
+if [ -z "$(docker images -q "$IMAGE")" ] \
+&& ! docker pull "$IMAGE"; then
+  echo "Failed to pull image: $IMAGE"
+  echo "Current container left intact: $CONT"
+  echo "The new bot was NOT started"
+  exit 126
+fi
+
+# kill currently running if any
+docker rm -f $CONT > /dev/null 2>&1
+
+# launch new
 docker run                        \
   --env ANARUCOMBOTTOKEN="$TOKEN" \
   --detach                        \
-  --hostname anarucombot          \
-  --name     anarucombot          \
+  --hostname $CONT                \
+  --name     $CONT                \
   --restart  always               \
-  danissimo/anarucombot:"$TAG"
+  $IMAGE
+RES=$?
+
+[ $RES -eq 0 ] && echo Success || echo Failure
+exit $RES
