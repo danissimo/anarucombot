604768be398f01b4fad54510dab89de143c9d3a0
danissimo <danis.tazeev@gmail.com>
2023-03-06 21:40:02 +0300

    shell scripts: home dir resolution refactored out into a function and improved

--- src/bin/container-run.sh
+++ src/bin/container-run.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 # rc: Run Container
 
-script="${BASH_SOURCE[0]}"
-while [ -L "$script" ]; do
-  ref="$(readlink "$script")"
-  if [[ "$ref" == /* ]]; then
-    script="$ref"
-  else
-    script="$(dirname "$script")/$ref"
-  fi
-done
+function locate-dir() {
+  local path="${BASH_SOURCE[0]}"
+  while [ -L "${path}" ]; do
+    local dir="$(cd -P -- "$(dirname -- "${path}")" >/dev/null 2>&1 && pwd)"
+    path="$(readlink "${path}")"
+    [[ "${path}" != /* ]] && path="${dir}/${path}"
+  done
+  path="$(readlink -f -- "${path}")"
+  dir="$(cd -P -- "$(dirname -- "${path}")" >/dev/null 2>&1 && pwd)"
+  echo "${dir}"
+}
 
 # home of the working directory of the repository
-home="$(dirname "$script")/../.."
+home="$(locate-dir)/../.."
 cd "$home" && src/docker/host/run-bot.sh latest
